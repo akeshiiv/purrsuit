fcec919b8cd2d42cf435e0c32de969faf494d1df
akeshiiv <limashlyn1004@gmail.com>
2026-05-31 18:05:14 +0800

    chore: test redeployment

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -90,7 +90,7 @@ export default function Dashboard() {
           <button onClick={handleStartStop} className="px-6 py-2 border rounded hover:bg-gray-200">
             {running ? 'Stop' : 'Start'}
           </button>
-          <button onClick={logout} className="px-6 py-2 border rounded hover:bg-gray-100">
+          <button onClick={logout} className="px-6 py-2 border rounded hover:bg-gray-200">
             Logout
           </button>
         </div>
